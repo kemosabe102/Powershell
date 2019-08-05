d58141f39412c2d9a852aa26366fc03c59d4c73f
Anthony Palermo <apalermo@visitpay.com>
2019-08-05 10:02:41 -0700

    performance update adding old file function

--- Modules/FileSystem.psm1
+++ Modules/FileSystem.psm1
@@ -6,9 +6,9 @@ function Archive-OldFolders {
     )
 
     try {
-        $SourceFolderObject = Get-Item -Path $Source
+        $SourceFolderObject = Get-Item -Path $Source | Where-Object {$_.LastWriteTime -lt $LastModified}
 
-        If ($SourceFolderObject.LastWriteTime -lt $LastModified)
+        If ($SourceFolderObject)
         {
             Robocopy "$Source" "$Destination" /MOVE /ZB /J /R:100 /MT:32 /FP /V
         }
@@ -25,12 +25,23 @@ function Archive-OldFolders {
     )
 
     try {
-        $TargetFolderObject = Get-Item -Path $Target
-        
-        If ($TargetFolderObject.LastWriteTime -lt $LastModified)
-        {
-            $TargetFolderObject | Remove-Item -Recurse -Force
-        }
+        $TargetFolderObjects = Get-Item -Path $Target | Where-Object {$_.LastWriteTime -lt $LastModified}
+        $TargetFolderObjects | Remove-Item -Recurse -Force
+    }
+    catch {
+        Write-Error ('{0} : {1} At Line:{2} char:{3}' -f $_.InvocationInfo.ScriptName, $_.Exception.Message, $_.InvocationInfo.ScriptLineNumber, $_.InvocationInfo.OffsetInLine)
+    }
+}
+
+function Remove-OldFiles {
+    param (
+        [Parameter(Mandatory=$True)] [string]$TargetDirectory,
+        [Parameter(Mandatory=$True)] [datetime]$LastModified
+    )
+
+    try {
+        $TargetFileObjects = Get-ChildItem -Path $TargetDirectory -Recurse -File | Where-Object {$_.LastWriteTime -lt $LastModified}
+        $TargetFileObjects | Remove-Item -Force
     }
     catch {
         Write-Error ('{0} : {1} At Line:{2} char:{3}' -f $_.InvocationInfo.ScriptName, $_.Exception.Message, $_.InvocationInfo.ScriptLineNumber, $_.InvocationInfo.OffsetInLine)
